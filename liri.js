89caa9243509cbff6d3c58e54c270f8c518426cb
Steven Brown <stevendbrown08@gmail.com>
2018-12-20 22:20:12 -0800

    log.txt cleared, slight inconsequential organizational change to liri.js

--- liri.js
+++ liri.js
@@ -38,11 +38,18 @@ const switchRead = (ans, str) => {
     }
 }
 
+//save data to log.txt
 const logData = str => {
     fs.appendFile("log.txt", str + " ~~~", function (err) {
         if (err)
             console.log(err);
     });
+}
+
+//log and console log data
+const report = str => {
+    logData(str);
+    console.log(str);
 }
 
 const concertThis = str => {
@@ -101,11 +108,6 @@ const concertSearch = str => {
                 report("Sorry, but we could not find any information on that artist.");
             }
         });
-}
-//log and console log data
-const report = str => {
-    logData(str);
-    console.log(str);
 }
 
 //pass search input onto spotifyThis
@@ -259,72 +261,3 @@ const readTxt = () => {
 
 //tries to get rid of quotes if there are some; does not work if quotes are in the middle of things
 const noMoreQuotes = x => { return x.substring(1, x.length - 1) };
-
-/*
-    LIRI COMMANDS:
-
-    * `node liri.js concert-this <artist/band name here>`
-
-        * This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
-
-            * Name of the venue
-
-            * Venue location
-
-            * Date of the Event (use moment to format this as "MM/DD/YYYY")
-
-    * `node liri.js spotify-this-song '<song name here>'`
-
-        * This will show the following information about the song in your terminal/bash window
-
-            * Artist(s)
-
-            * The song's name
-
-            * A preview link of the song from Spotify
-
-            * The album that the song is from
-
-        * If no song is provided then your program will default to "The Sign" by Ace of Base.
-
-        * You will utilize the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) package in order to retrieve song information from the Spotify API.
-
-    * `node liri.js movie-this '<movie name here>'`
-
-        * This will output the following information to your terminal/bash window:
-
-            ```
-            * Title of the movie.
-            * Year the movie came out.
-            * IMDB Rating of the movie.
-            * Rotten Tomatoes Rating of the movie.
-            * Country where the movie was produced.
-            * Language of the movie.
-            * Plot of the movie.
-            * Actors in the movie.
-            ```
-
-            * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-            * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
-
-                * It's on Netflix!
-
-            * You'll use the `axios` package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use `trilogy`.
-
-    * `node liri.js do-what-it-says`
-
-        * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-        * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-
-        * Edit the text in random.txt to test out the feature for movie-this and concert-this.
-
-    BONUS
-
-        * In addition to logging the data to your terminal/bash window, output the data to a .txt file called `log.txt`.
-
-        * Make sure you append each command you run to the `log.txt` file.
-
-        * Do not overwrite your file each time you run a command.
-*/
